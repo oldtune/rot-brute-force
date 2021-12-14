3c4c1528209b5404444ef84eb2831ae5592e1858
QuackQuack <vivaladaman@gmail.com>
2021-12-14 07:50:27 +0700

    Done ROT but haven't tested yet. will test later

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,31 @@
 use std::collections::HashMap;
 
 use structopt::StructOpt;
-#[macro_use]
-extern crate lazy_static;
-lazy_static! {
-    static ref char_map: HashMap<char, u8> = HashMap::from([
-        ('a', 1),
-        ('b', 2),
-        ('c', 3),
-        ('d', 4),
-        ('e', 5),
-        ('f', 6),
-        ('g', 7),
-        ('h', 8),
-        ('i', 9),
-        ('j', 10),
-        ('k', 11),
-        ('l', 12),
-        ('m', 13),
-        ('n', 14),
-        ('o', 15),
-        ('p', 16),
-        ('q', 17),
-        ('r', 18),
-        ('s', 19),
-        ('t', 20),
-        ('u', 21),
-        ('v', 22),
-        ('w', 23),
-        ('x', 24),
-        ('y', 25),
-        ('z', 26),
-    ]);
-}
+
 fn main() {
     let cli = Cli::from_args();
+    for i in 1..14 {
+        println!("{}", rot(i, &cli.input));
+    }
 }
 
-fn rot(number: u8, string: &String) {
+fn rot(number: u8, string: &String) -> String {
     let mut result = String::new();
 
-    for (_, c) in string.chars().enumerate() {
-        let char_in_map = char_map[c];
+    for c in string.chars() {
+        let code_point = c as u8;
+        let mutated_char: u8;
+        if code_point >= 97 && code_point <= 122 {
+            mutated_char = (code_point + number) % 97 % 26 + 97;
+        } else if (code_point >= 65 && code_point <= 90) {
+            mutated_char = (code_point + number) % 65 % 26 + 65;
+        } else {
+            mutated_char = code_point;
+        }
+        result.push(mutated_char as char);
     }
+
+    result
 }
 
 #[derive(StructOpt)]
